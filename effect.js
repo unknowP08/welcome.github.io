f9ea03d172c53f98c27b2a60ed8cf2cd8d71f884
alexliqu09 <alexander.lique.l@uni.pe>
2024-08-03 13:26:08 -0300

    quit cake

--- effect.js
+++ effect.js
@@ -6,13 +6,13 @@ $('document').ready(function(){
 		var vw;
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
-			$('#b1,#b2,#b3,#b4,#b5,#b6').stop();
+			$('#b1,#b2,#b3').stop();
 			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-250},500);
 			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
+			//$('#b44').animate({top:240, left: vw-50},500);
+			//$('#b55').animate({top:240, left: vw+50},500);
+			//$('#b66').animate({top:240, left: vw+150},500);
 			//$('#b77').animate({top:240, left: vw+250},500);
 		});
 
